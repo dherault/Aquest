241643c438707661ef15deec55b96e7d02dd96ad
David Hérault <dherault@gmail.com>
2017-07-12 15:17:37 +0200

    add Commit and commit input

--- front/src/User.js
+++ front/src/User.js
@@ -3,25 +3,64 @@ import { createFragmentContainer, graphql } from 'react-relay';
 import './User.css';
 
 class User extends Component {
+  state = {}
+
+  updateState = key => e => this.setState({ [key]: e.target.value })
+
+  submitCommit = e => {
+    e.preventDefault();
+  }
+
+  componentWillMount() {
+    if (!this.props.user.acquiredSkills.edges.length) return;
+
+    this.setState({
+      commitDescription: '',
+      commitSkillId: this.props.user.acquiredSkills.edges[0].node.id,
+    });
+  }
+
   render() {
     const { user } = this.props;
-
-    if (!user) return console.log('no User props') || null;
+    const { commitDescription, commitSkillId } = this.state;
+    const acquiredSkillNodes = user.acquiredSkills.edges.map(e => e.node);
 
     return (
       <div className="User">
-        <img className="User-picture" alt="" src={user.pictureUrl} />
+        <section>
+          <img className="User-picture" alt="" src={user.pictureUrl} />
 
-        <h1>{user.fisrtName} {user.lastName}</h1>
+          <h1>{user.fisrtName} {user.lastName}</h1>
 
-        <p className="User-intro">
-          {user.intro}
-        </p>
+          <p className="User-intro">
+            {user.intro}
+          </p>
+        </section>
 
-        <h2>Skill list</h2>
-        <ul>
-          {user.acquiredSkills.edges.map(({ node }) => node.label)}
-        </ul>
+        <section>
+          <h2>Skill list</h2>
+          <ul>
+            {acquiredSkillNodes.map(n => n.label)}
+          </ul>
+        </section>
+
+        {!!acquiredSkillNodes.length && (
+          <section>
+            <form onSubmit={this.submitCommit}>
+              <input type="text" value={commitDescription} onChange={this.updateState('commitDescription')}/>
+              <select value={commitSkillId} onChange={this.updateState('commitSkillId')}>
+                {acquiredSkillNodes.map(n => (
+                  <option key={n.id} value={n.id}>
+                    {n.label}
+                  </option>
+                ))}
+              </select>
+              <input type="submit" value="Commit" />
+            </form>
+          </section>
+        )}
+
+
       </div>
     );
   }
