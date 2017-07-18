25dd3e36b9a79402f85edfc0006e54f86840972c
David Hérault <dherault@gmail.com>
2017-07-18 18:19:52 +0200

    toward skillInstaces

--- services/graphql/src/customFields.js
+++ services/graphql/src/customFields.js
@@ -10,5 +10,5 @@ _.addFieldOnObjectType('http://foo.com#Thing', 'iri', {
 
 _['http://www.w3.org/2000/01/rdf-schema#label'].isGraphqlList = false;
 
-_['http://foo.com#skills'].isRelayConnection = true;
+_['http://foo.com#skillInstances'].isRelayConnection = true;
 _['http://foo.com#commits'].isRelayConnection = true;
---
+++ services/graphql/src/mutations/createSkillInstance.js
@@ -0,0 +1,45 @@
+const { GraphQLNonNull, GraphQLString } = require('graphql');
+const { mutationWithClientMutationId, cursorForObjectInConnection } = require('graphql-relay');
+const createResource = require('../utils/createResource');
+const ensureAuth = require('../utils/ensureAuth');
+const db = require('../db');
+const { run } = require('../db/queries');
+const _ = require('../graph');
+
+module.exports = mutationWithClientMutationId({
+  name: 'CreateSkillInstance',
+  inputFields: {
+    skillId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  outputFields: {
+    skillInstanceEdge: {
+      type: _.getEdgeType('http://foo.com#SkillInstance'),
+      resolve: ({ skillInstance: { id } }, args, { user }) => {
+        const query = db.createQuery('SkillInstance').filter('sourcePerson', user.id);
+
+        return run(query).then(skillInstances => {
+          const skillInstance = skillInstances.find(s => s.id === id);
+
+          return {
+            cursor: cursorForObjectInConnection(skillInstances, skillInstance),
+            node: skillInstance,
+          };
+        });
+      },
+    },
+    user: {
+      type: _.getObjectType('http://foo.com#Person'),
+      resolve: (payload, args, { user }) => user,
+    },
+  },
+  mutateAndGetPayload: ensureAuth(({ skillId }, context) => {
+    const skillInstance = createResource('SkillInstance', context, {
+      skill: skillId,
+      level: 1,
+    });
+
+    return db.upsertResource(skillInstance).then(() => ({ skillInstance }));
+  }),
+});
