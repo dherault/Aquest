a96952d5bd029abb2d9b13d1a45d3e0293efd2c0
David Hérault <dherault@gmail.com>
2017-07-26 17:22:45 +0200

    create UpdateViewerMutation on front side

---
+++ front/src/mutations/UpdateViewerMutation.js
@@ -0,0 +1,35 @@
+import { commitMutation, graphql } from 'react-relay';
+import environment from '../relayEnvironment';
+
+const mutation = graphql`
+  mutation UpdateViewerMutation($input: UpdateViewerInput!) {
+    updateViewer(input: $input) {
+      viewer {
+        id
+        email
+        pseudo
+        description
+        hasPrivateProfile
+        wantsToBeShowcased
+      }
+    }
+  }
+`;
+
+const updateViewer = input => commitMutation(environment, {
+  mutation,
+  variables: {
+    input: {
+      ...input,
+      clientMutationId: Math.random().toString().slice(2),
+    },
+  },
+  onCompleted(response, errors) {
+    console.log('errors:', errors);
+  },
+  onError(error) {
+    console.error('error:', error);
+  },
+});
+
+export default updateViewer;
