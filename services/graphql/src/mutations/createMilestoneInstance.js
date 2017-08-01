5cda4a3913b15f9129d72de0db393556c152fafb
David Hérault <dherault@gmail.com>
2017-08-01 18:55:31 +0200

    add CreateMilestoneInstance graphQL mutation

---
+++ services/graphql/src/mutations/createMilestoneInstance.js
@@ -0,0 +1,43 @@
+const { GraphQLNonNull, GraphQLString } = require('graphql');
+const { mutationWithClientMutationId, cursorForObjectInConnection } = require('graphql-relay');
+const createResourceObject = require('../utils/createResourceObject');
+const ensureAuth = require('../utils/ensureAuth');
+const { query, createResource } = require('../db');
+const _ = require('../graph');
+
+module.exports = mutationWithClientMutationId({
+  name: 'CreateMilestoneInstance',
+  inputFields: {
+    milestoneId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  outputFields: {
+    milestoneInstanceEdge: {
+      type: _.getEdgeType('http://foo.com#MilestoneInstance'),
+      resolve: ({ milestoneInstance: { id } }, args, { viewer }) => query(db => db
+        .collection('MilestoneInstance')
+        .find({ sourceUser: viewer.id })
+        .toArray()
+      ).then(milestoneInstances => {
+        const milestoneInstance = milestoneInstances.find(s => s.id === id);
+
+        return {
+          cursor: cursorForObjectInConnection(milestoneInstances, milestoneInstance),
+          node: milestoneInstance,
+        };
+      }),
+    },
+    viewer: {
+      type: _.getObjectType('http://foo.com#User'),
+      resolve: (payload, args, { viewer }) => viewer,
+    },
+  },
+  mutateAndGetPayload: ensureAuth(({ milestoneId }, context) => {
+    const milestoneInstance = createResourceObject('MilestoneInstance', context, {
+      milestone: milestoneId,
+    });
+
+    return createResource(milestoneInstance).then(() => ({ milestoneInstance }));
+  }),
+});
